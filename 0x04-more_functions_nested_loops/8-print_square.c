7eb22cec0beb971a09e5746f7c978a68063d4486
AisienJohn1 <107049141+AisienJohn1@users.noreply.github.com>
2022-06-10 05:05:14 +0100

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - tests function
+ * @size: value inputted
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
